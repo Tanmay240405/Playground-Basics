74f7809297f7cddccb7daf93f3b9642a8e452230
SDC-USER <tanmaysaxena2404@gmail.com>
2025-08-28 12:09:21 +0530

    gaurd

--- Gaurd.playground/Contents.swift
+++ Gaurd.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-//Gaurd -> proper tracking of your code -> when we a=want a early exit
+//Gaurd -> proper tracking of your code -> when we want an early exit -> opposite of "if"
 
 var birthdayIdToday = true
-var invitedGuest = ["Guest"]
+var invitedGuest = ["Guest1"]
 var cakeCandle = true
 @MainActor
 func sing() {
@@ -23,3 +23,26 @@ func sing() {
 }
 
 sing()
+
+func Book(title: String?, price: Double?, pages: Int?) {
+    guard let theTitle = title,
+          let thePrice = price,
+          let thePages = pages else {
+        print("Missing Name, Price or Pages")
+        return
+    }
+    print("\(theTitle) cost \(thePrice) and has \(thePages) pages")
+}
+
+Book(title: nil, price: 100, pages: 100)
+
+func Divide(number: Int, divisor: Int) {
+    guard divisor != 0 else {
+        print("Divisor is zero")
+        return
+    }
+    print("Answer is \(number / divisor)")
+}
+
+Divide(number: 10, divisor: 2)
+Divide(number: 10, divisor: 0)
